42846b894651ba6dacca5f1730addc259c0ca50f
Maxximusan <103362267+Maxximusan@users.noreply.github.com>
2024-03-11 18:34:42 +0200

    tailwind - продолжение +
    - настройки в setting.json для игнора ошибок
    - мои шрифты: по умолчанию и дополнительный

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,8 @@
 module.exports = {
   content: ["./index.html"],
   theme: {
+   
+
     extend: {
       backgroundImage: {
         'hero-pattern': "linear-gradient(to right, rgba(47, 48, 58, 0.4), rgba(47, 48, 58, 0.4)),url('/images/1590125748_05-laptop-for-coding.jpg')",
@@ -13,6 +15,15 @@ module.exports = {
         border: '#f36421',
         bgrHead: '#87775bde',
         bgrBody: '#c5c5c5',
+        bgrAbout: '#ffd87fb8',
+        bgrPortfolio: '#a6d3b6',
+        bgrContacts: '#bdb76b99',
+        h2: '#228b22',
+      },
+
+      fontFamily: {
+        mons: ['Montserrat', 'monospace'],
+        sans: ['Roboto', 'sans-serif'],
       },
     },
   },
