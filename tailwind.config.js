b2b5ff58d37a51d35d780679559184ee28af4360
Maxximusan <103362267+Maxximusan@users.noreply.github.com>
2024-03-12 21:30:19 +0200

    tailwind -\\-

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,10 +5,12 @@ module.exports = {
    
 
     extend: {
+
       backgroundImage: {
         'hero-pattern': "linear-gradient(to right, rgba(47, 48, 58, 0.4), rgba(47, 48, 58, 0.4)),url('/images/1590125748_05-laptop-for-coding.jpg')",
         
       },
+
       colors:{
         h1: '#af4e0a',
         bgrImg: '#252222',
@@ -19,11 +21,24 @@ module.exports = {
         bgrPortfolio: '#a6d3b6',
         bgrContacts: '#bdb76b99',
         h2: '#228b22',
+        bgrProjItem: '#c7b7a5',
+        bgrProjItem2: '#bbb0d5',
+        h4: '#0000ff',
+        gitLink: '#0b7c14',
+        telegram: 'rgb(4, 163, 255)',
       },
 
       fontFamily: {
         mons: ['Montserrat', 'monospace'],
         sans: ['Roboto', 'sans-serif'],
+      },
+
+      spacing: {
+        '400': '25rem',
+      },
+
+      boxShadow: {
+        'forProjects': '0px 1px 1px rgba(0, 0, 0, 0.12), 0px 4px 4px rgba(0, 0, 0, 0.06), 1px 4px 6px rgba(0, 0, 0, 0.16)',
       },
     },
   },
