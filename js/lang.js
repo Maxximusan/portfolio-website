03736f9aa3865e04087f0feac0a53cf2f693691e
Maxximusan <103362267+Maxximusan@users.noreply.github.com>
2024-10-17 00:12:35 +0300

    поправки описания парочки проектов ....
    - а также тут какието изменения стилей - но я не помню где это (возможно связано с pdf)

--- js/lang.js
+++ js/lang.js
@@ -1,183 +1,181 @@
 export const langArray = {
-
-    'пример' : {
-        'ru': '',
-        'en': '',
-        'ua': '',
-    },
-
-  'title' : {
-    'ru': 'Мой сайт-портфолио',
-    'en': 'My portfolio website',
-    'ua': 'Мiй сайт-портфолiо',
+  пример: {
+    ru: '',
+    en: '',
+    ua: '',
   },
-    'nv-about' : {
-        'ru': 'Обо мне',
-        'en': 'About',
-        'ua': 'Про мене',
-    },
 
-    'nv-portfolio' : {
-        'ru': 'Портфолио',
-        'en': 'Portfolio',
-        'ua': 'Портфоліо',
-    },
+  title: {
+    ru: 'Мой сайт-портфолио',
+    en: 'My portfolio website',
+    ua: 'Мiй сайт-портфолiо',
+  },
+  'nv-about': {
+    ru: 'Обо мне',
+    en: 'About',
+    ua: 'Про мене',
+  },
 
-    'nv-contacts' : {
-        'ru': 'Контакты',
-        'en': 'Contacts',
-        'ua': 'Контакти',
-    },
+  'nv-portfolio': {
+    ru: 'Портфолио',
+    en: 'Portfolio',
+    ua: 'Портфоліо',
+  },
 
-    'h1' : {
-        'ru': 'Максим Чуйков',
-        'en': 'Maksym Chuikov',
-        'ua': 'Максим Чуйков',
-    },
+  'nv-contacts': {
+    ru: 'Контакты',
+    en: 'Contacts',
+    ua: 'Контакти',
+  },
 
-    'about-title' : {
-        'ru': 'Всем привет! Я Максим Чуйков, фронтенд разработчик из Харькова. Я ищу свою первую работу.',
-        'en': 'Hi there! I\'m a Maksim Chuikov, front end developer based on Kharkov. I am looking for my first job.',
-        'ua': 'Всім привіт! Я Максим Чуйков, фронтенд розробник із Харкова. Я шукаю свою першу працю.',
-    },
-    'about-experiance' : {
-        'ru': 'В прошлом году я закончил курсы фул-стек:',
-        'en': 'Last year i have ended full-stack courses:',
-        'ua': 'Минулого року я закінчив курси фул-стеку: ',
-    },
-    'about-looking' : {
-        'ru': 'Имею опыт реализации индивидуальных и командных проектов, с которыми вы можете ознакомиться в разделе ниже. Сейчас я продолжаю учиться самостоятельно и также прогрессирую в английском языке.',
-        'en': ' I have experience in realization individual and team projects, that you can see in section below. Now i keep studying on my own, and progresing in english too. ',
-        'ua': 'Маю досвід реалізації індивідуальних та командних проектів, з якими ви можете ознайомитись у розділі нижче. Зараз я продовжую вчитися самостійно та також прогресую в англійській мові.',
-    },
+  h1: {
+    ru: 'Максим Чуйков',
+    en: 'Maksym Chuikov',
+    ua: 'Максим Чуйков',
+  },
 
+  'about-title': {
+    ru: 'Всем привет! Я Максим Чуйков, фронтенд разработчик из Харькова. Я ищу свою первую работу.',
+    en: "Hi there! I'm a Maksim Chuikov, front end developer based on Kharkov. I am looking for my first job.",
+    ua: 'Всім привіт! Я Максим Чуйков, фронтенд розробник із Харкова. Я шукаю свою першу працю.',
+  },
+  'about-experiance': {
+    ru: 'В прошлом году я закончил курсы фул-стек:',
+    en: 'Last year i have ended full-stack courses:',
+    ua: 'Минулого року я закінчив курси фул-стеку: ',
+  },
+  'about-looking': {
+    ru: 'Имею опыт реализации индивидуальных и командных проектов, с которыми вы можете ознакомиться в разделе ниже. Сейчас я продолжаю учиться самостоятельно и также прогрессирую в английском языке.',
+    en: ' I have experience in realization individual and team projects, that you can see in section below. Now i keep studying on my own, and progresing in english too. ',
+    ua: 'Маю досвід реалізації індивідуальних та командних проектів, з якими ви можете ознайомитись у розділі нижче. Зараз я продовжую вчитися самостійно та також прогресую в англійській мові.',
+  },
 
-    'team-projects' : {
-        'ru': 'Командные проекты',
-        'en': 'Team projects',
-        'ua': 'Командні проекти',
-    },
+  'team-projects': {
+    ru: 'Командные проекты',
+    en: 'Team projects',
+    ua: 'Командні проекти',
+  },
 
-    'ice-cream-title' : {
-        'ru': 'Мороженое',
-        'en': 'Ice-cream',
-        'ua': 'Морозиво',
-    },
+  'ice-cream-title': {
+    ru: 'Мороженое',
+    en: 'Ice-cream',
+    ua: 'Морозиво',
+  },
 
-    'filmoteka-title' : {
-        'ru': 'Фильмотека',
-        'en': 'Filmoteka',
-        'ua': 'Фільмотека',
-    },
-    'petly-service-title' : {
-        'ru': 'Обслуживание домашних животных',
-        'en': 'Petly-service',
-        'ua': 'Обслуговування домашніх тварин',
-    },
-    'livePageLink' : {
-        'ru': 'Живая страница',
-        'en': 'Live Page',
-        'ua': 'Жива сторінка',
-    },
-    'GithubLink' : {
-        'ru': 'Гитхаб',
-        'en': 'Github',
-        'ua': 'Гітхаб',
-    },
-    'ice-cream-description' : {
-        'ru': 'Учебный групповой проект после курса html/css. Сайт компании по производству мороженого с формой заказа, подробной информацией о продукции и картой точек продаж. Сайт с адаптивной версткой. Я создал раздел "товары" и анимацию для этого раздела.',
-        'en': `Educational group project after html/css course. The ice-cream company's website, with an order form, product details and a map of sales outlets. The website with a responsive layout. I created a section products, and animation for it.`,
-        'ua': 'Навчальний груповий проект після курсу html/css. Сайт компанії з виробництва морозива з формою замовлення, детальною інформацією про продукцію та карту точок продажу. Сайт із адаптивною версткою. Я створив розділ "товари" та анімацію для цього розділу',
-    },
-    'filmoteka-description' : {
-        'ru': 'Учебный групповой проект после курса javascript. Кинопоиск с возможностью прочитать информацию о фильмах, посмотреть трейлер и добавить фильмы в библиотеку. Я создал функционал для получения данных о фильмах из API, отрисовку их карточек и отрисовку в модальном окне.',
-        'en': 'Educational group project after javascript course. A film search site with the ability to read information about the films, watch a trailer and add films to the library. I created functionality for obtaining movie data from the API, rendering their cards and rendering in a modal window.',
-        'ua': 'Навчальний груповий проект післе курсу javascript. Кінопошук із можливістю прочитати інформацію про фільми, подивитися трейлер та додати фільми до бібліотеки. Я створив функціонал для отримання даних про фільми з API, малювання їх карток та малювання в модальному вікні.',
-    },
-    'petly-services-description' : {
-        'ru': 'Учебный групповой проект после курса react/node. Приложение сервиса объявлений по адаптации домашних животных и сопутствующих новостей и партнеров. Имеет личный профиль пользователя с возможностью вести историю своего питомца. Я создал страницу поиска домашних животных, которая также функциональна для этой страницы с руководителем команды.',
-        'en': 'Educational group project after react/node course. The application of the service of announcements for the adaptation of pets and related news and partners. Has a personal user profile with the ability to keep a history of your pet. I created a find-pet page, also functional for this page with team-lead.',
-        'ua': 'Навчальний груповий проект після курсу react/node. Додаток сервісу оголошень з адаптації домашніх тварин та супутніх новин та партнерів. Має особистий профіль користувача з можливістю вести історію свого вихованця. Я створив сторінку пошуку домашніх тварин, яка також функціональна для цієї сторінки з керівником команди.',
-    },
-    'individual-projects' : {
-        'ru': 'Индивидуальные проекты',
-        'en': 'Individual projects',
-        'ua': 'Індивідуальні проекти',
-    },
-    'web-stuidio-title' : {
-        'ru': 'Веб-студия',
-        'en': 'Web-stuidio',
-        'ua': 'Веб студія',
-    },
-    'web-stuidio-description' : {
-        'ru': 'Учебный проект в рамках курса html & css. Подробности вы можете посмотреть по ссылке ниже в «readmy»',
-        'en': 'Educational project as part of the html & css course. Details you can see at the link below in «readmy»',
-        'ua': 'Навчальний проект у рамках курсу html & css. Подробиці ви можете переглянути за посиланням нижче у «readmy»',
-    },
-    'phonebook-title' : {
-        'ru': 'Телефонная книга',
-        'en': 'Phonebook',
-        'ua': 'Телефонна книга',
-    },
-    'phonebook-description' : {
-        'ru': 'Учебный проект в рамках курса React. Улучшен после. Подробности вы можете посмотреть по ссылке ниже в «readmy»',
-        'en': 'Educational project as part of the react course. Improved after. Details you can see at the link below in «readmy»',
-        'ua': 'Навчальний проект у рамках курсу React. Поліпшено після. Подробиці ви можете переглянути за посиланням нижче у «readmy»',
-    },
-    'search-movies-title' : {
-        'ru': 'Поиск фильмов',
-        'en': 'Search movies',
-        'ua': 'Пошук фільмів',
-    },
-    'search-movies-description' : {
-        'ru': 'Учебный проект в рамках курса React. Улучшен после. Подробности вы можете посмотреть по ссылке ниже в «readmy»',
-        'en': 'Educational project as part of the react course. Improved after. Details you can see at the link below in «readmy»',
-        'ua': 'Навчальний проект у рамках курсу React. Поліпшено після. Подробиці ви можете переглянути за посиланням нижче у «readmy»',
-    },
-    'rn-project-tittle' : {
-        'ru': '',
-        'en': 'React Native Project',
-        'ua': '',
-    },
-    'rn-proj-description' : {
-        'ru': 'Учебный проект в рамках дополнительного курса React-native. Подробности, и как открыть это приложение - вы можете посмотреть по ссылке ниже в «readmy»',
-        'en': 'Educational project as part of the react-native additional course. Details, and how open this app - you can see at the link below in «readmy».',
-        'ua': 'Навчальний проект у рамках додаткового курсу React-Native. Подробиці, і як відкрити цей додаток - ви можете подивитися за посиланням нижче в «readmy»',
-    },
-    'find-your-favorite-cats' : {
-        'ru': 'Найди своего любимого котэ',
-        'en': 'Find your favorite cats',
-        'ua': 'Знайди свою улюблену та няшну кицю',
-    },
-    'cats-description' : {
-        'ru': 'В этом проекте я повторил некоторые моменты, изученные ранее, попытался разобраться в новых библиотеках и многом другом. Он еще не закончен. Позже здесь также будет бэкэнд',
-        'en': 'At this project i repeated some had learned moments earlier, tryed to figure out in new libraries and other things. It has not ended yet. Later will be backend here too.',
-        'ua': 'У цьому проекті я повторив деякі моменти, вивчені раніше, спробував розібратися в нових бібліотеках та багато іншого. Він ще не закінчен. Пізніше тут також буде бекенд',
-    },
-    'rock-stars-singers' : {
-        'ru': 'Вокалисты Рок-звёзды',
-        'en': 'Rock stars singers',
-        'ua': 'Вокалісти Рок-зірки',
-    },
-    'singers-description' : {
-        'ru': 'Тестовое задание, и в текущей версии используется type script. Подробности вы можете посмотреть по ссылке ниже в «readmy»',
-        'en': 'The test task, and at current version uses type script. Details you can see at the link below in «readmy»',
-        'ua': 'Тестове завдання і в поточній версії використовується type script. Подробиці ви можете переглянути за посиланням нижче у «readmy»',
-    },
-    'my-contacts-title' : {
-        'ru': 'Мои контакты',
-        'en': 'My contacts',
-        'ua': 'Мої контакти',
-    },
+  'filmoteka-title': {
+    ru: 'Фильмотека',
+    en: 'Filmoteka',
+    ua: 'Фільмотека',
+  },
+  'petly-service-title': {
+    ru: 'Обслуживание домашних животных',
+    en: 'Petly-service',
+    ua: 'Обслуговування домашніх тварин',
+  },
+  livePageLink: {
+    ru: 'Живая страница',
+    en: 'Live Page',
+    ua: 'Жива сторінка',
+  },
+  GithubLink: {
+    ru: 'Гитхаб',
+    en: 'Github',
+    ua: 'Гітхаб',
+  },
+  'ice-cream-description': {
+    ru: 'Учебный групповой проект после курса html/css. Сайт компании по производству мороженого с формой заказа, подробной информацией о продукции и картой точек продаж. Сайт с адаптивной версткой. Я создал раздел "товары" и анимацию для этого раздела.',
+    en: `Educational group project after html/css course. The ice-cream company's website, with an order form, product details and a map of sales outlets. The website with a responsive layout. I created a section products, and animation for it.`,
+    ua: 'Навчальний груповий проект після курсу html/css. Сайт компанії з виробництва морозива з формою замовлення, детальною інформацією про продукцію та карту точок продажу. Сайт із адаптивною версткою. Я створив розділ "товари" та анімацію для цього розділу',
+  },
+  'filmoteka-description': {
+    ru: 'Учебный групповой проект после курса javascript. Кинопоиск с возможностью прочитать информацию о фильмах, посмотреть трейлер и добавить фильмы в библиотеку. Я создал функционал для получения данных о фильмах из API, отрисовку их карточек и отрисовку в модальном окне.',
+    en: 'Educational group project after javascript course. A film search site with the ability to read information about the films, watch a trailer and add films to the library. I created functionality for obtaining movie data from the API, rendering their cards and rendering in a modal window.',
+    ua: 'Навчальний груповий проект післе курсу javascript. Кінопошук із можливістю прочитати інформацію про фільми, подивитися трейлер та додати фільми до бібліотеки. Я створив функціонал для отримання даних про фільми з API, малювання їх карток та малювання в модальному вікні.',
+  },
+  'petly-services-description': {
+    ru: 'Учебный групповой проект после курса react/node. Приложение сервиса объявлений по адаптации домашних животных и сопутствующих новостей и партнеров. Имеет личный профиль пользователя с возможностью вести историю своего питомца. Я создал страницу поиска домашних животных, которая также функциональна для этой страницы с руководителем команды.',
+    en: 'Educational group project after react/node course. The application of the service of announcements for the adaptation of pets and related news and partners. Has a personal user profile with the ability to keep a history of your pet. I created a find-pet page, also functional for this page with team-lead.',
+    ua: 'Навчальний груповий проект після курсу react/node. Додаток сервісу оголошень з адаптації домашніх тварин та супутніх новин та партнерів. Має особистий профіль користувача з можливістю вести історію свого вихованця. Я створив сторінку пошуку домашніх тварин, яка також функціональна для цієї сторінки з керівником команди.',
+  },
+  'individual-projects': {
+    ru: 'Индивидуальные проекты',
+    en: 'Individual projects',
+    ua: 'Індивідуальні проекти',
+  },
+  'web-stuidio-title': {
+    ru: 'Веб-студия',
+    en: 'Web-stuidio',
+    ua: 'Веб студія',
+  },
+  'web-stuidio-description': {
+    ru: 'Учебный проект в рамках курса html & css. Подробности вы можете посмотреть по ссылке ниже в «readmy»',
+    en: 'Educational project as part of the html & css course. Details you can see at the link below in «readmy»',
+    ua: 'Навчальний проект у рамках курсу html & css. Подробиці ви можете переглянути за посиланням нижче у «readmy»',
+  },
+  'phonebook-title': {
+    ru: 'Телефонная книга',
+    en: 'Phonebook',
+    ua: 'Телефонна книга',
+  },
+  'phonebook-description': {
+    ru: 'Учебный проект в рамках курса React. Улучшен после. Работает с моим личным бэкендом. Подробности вы можете посмотреть по ссылке ниже в «readmy»',
+    en: 'Educational project as part of the react course. Improved after. Works with my personal backend. Details you can see at the link below in «readmy»',
+    ua: 'Навчальний проект у рамках курсу React. Поліпшено після. Працює з моїм особистим бекендом. Подробиці ви можете переглянути за посиланням нижче у «readmy»',
+  },
+  'search-movies-title': {
+    ru: 'Поиск фильмов',
+    en: 'Search movies',
+    ua: 'Пошук фільмів',
+  },
+  'search-movies-description': {
+    ru: 'Учебный проект в рамках курса React. Улучшен после. Подробности вы можете посмотреть по ссылке ниже в «readmy»',
+    en: 'Educational project as part of the react course. Improved after. Details you can see at the link below in «readmy»',
+    ua: 'Навчальний проект у рамках курсу React. Поліпшено після. Подробиці ви можете переглянути за посиланням нижче у «readmy»',
+  },
+  'rn-project-tittle': {
+    ru: '',
+    en: 'React Native Project',
+    ua: '',
+  },
+  'rn-proj-description': {
+    ru: 'Учебный проект в рамках дополнительного курса React-native. Подробности, и как открыть это приложение - вы можете посмотреть по ссылке ниже в «readmy»',
+    en: 'Educational project as part of the react-native additional course. Details, and how open this app - you can see at the link below in «readmy».',
+    ua: 'Навчальний проект у рамках додаткового курсу React-Native. Подробиці, і як відкрити цей додаток - ви можете подивитися за посиланням нижче в «readmy»',
+  },
+  'find-your-favorite-cats': {
+    ru: 'Найди своего любимого котэ',
+    en: 'Find your favorite cats',
+    ua: 'Знайди свою улюблену та няшну кицю',
+  },
+  'cats-description': {
+    ru: 'В этом проекте я повторил некоторые моменты, изученные ранее, попытался разобраться в новых библиотеках и многом другом. Работает с моим личным бэкендом. Подробности вы можете посмотреть по ссылке ниже в «readmy»',
+    en: 'At this project i repeated some had learned moments earlier, tryed to figure out in new libraries and other things. Works with my personal backend. Details you can see at the link below in «readmy»',
+    ua: 'У цьому проекті я повторив деякі моменти, вивчені раніше, спробував розібратися в нових бібліотеках та багато іншого. Працює з моїм особистим бекендом. Подробиці ви можете переглянути за посиланням нижче у «readmy» ',
+  },
+  'rock-stars-singers': {
+    ru: 'Вокалисты Рок-звёзды',
+    en: 'Rock stars singers',
+    ua: 'Вокалісти Рок-зірки',
+  },
+  'singers-description': {
+    ru: 'Тестовое задание, и в текущей версии используется type script. Подробности вы можете посмотреть по ссылке ниже в «readmy»',
+    en: 'The test task, and at current version uses type script. Details you can see at the link below in «readmy»',
+    ua: 'Тестове завдання і в поточній версії використовується type script. Подробиці ви можете переглянути за посиланням нижче у «readmy»',
+  },
+  'my-contacts-title': {
+    ru: 'Мои контакты',
+    en: 'My contacts',
+    ua: 'Мої контакти',
+  },
 
-    'telega' : {
-        'ru': 'Телеграм',
-        'en': 'Telegram',
-        'ua': 'Телеграм',
-    },
-    'linkdin' : {
-        'ru': 'Линкидин',
-        'en': 'Linkdin',
-        'ua': 'Лінкідін',
-    },
-}
+  telega: {
+    ru: 'Телеграм',
+    en: 'Telegram',
+    ua: 'Телеграм',
+  },
+  linkdin: {
+    ru: 'Линкидин',
+    en: 'Linkdin',
+    ua: 'Лінкідін',
+  },
+};
